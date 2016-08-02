25d6611184989b4b855cb7f48467929f42262747
llblives04 <llblives04@gmail.com>
2016-08-02 11:21:28 -0400

    yup

--- Final/myscript.js
+++ Final/myscript.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     /* Check the location of each desired element */
     $('.hideme').each(function(i) {
 
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight()
       var bottom_of_window = $(window).scrollTop() + $(window).height();
 
       /* If the object is completely visible in the window, fade it it */
@@ -14,7 +14,7 @@ $(document).ready(function() {
 
         $(this).animate({
           'opacity': '1'
-        }, 100);
+        }, 500);
 
       }
 
